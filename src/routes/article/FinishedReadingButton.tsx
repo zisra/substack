fbd864943ff77ee516504833c2929064e579bd14
Isra <byzkk@protonmail.com>
2025-02-19 00:44:39 +0800

    Remove FinishedReadingButton from print

--- src/routes/article/FinishedReadingButton.tsx
+++ src/routes/article/FinishedReadingButton.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import type { Database } from '@/lib/database';
 import type { ArticleSaved } from '@/lib/types';
+import { Separator } from '@radix-ui/react-select';
 import { ArchiveIcon, ArchiveXIcon } from 'lucide-react';
 
 export function FinishedReadingButton({
@@ -13,28 +14,31 @@ export function FinishedReadingButton({
 	db: Database;
 }) {
 	return (
-		<div className='text-center pt-4'>
-			{!article.archived ? (
-				<Button
-					onClick={async () => {
-						await db.open();
-						db.archiveArticle(article.url);
-						setArticle({ ...article, archived: true });
-					}}
-				>
-					<ArchiveIcon /> Finished reading
-				</Button>
-			) : (
-				<Button
-					onClick={async () => {
-						await db.open();
-						db.unArchiveArticle(article.url);
-						setArticle({ ...article, archived: false });
-					}}
-				>
-					<ArchiveXIcon /> Continue reading
-				</Button>
-			)}
-		</div>
+		<>
+			<Separator className="my-2" />
+			<div className="text-center pt-4 print:hidden">
+				{!article.archived ? (
+					<Button
+						onClick={async () => {
+							await db.open();
+							db.archiveArticle(article.url);
+							setArticle({ ...article, archived: true });
+						}}
+					>
+						<ArchiveIcon /> Finished reading
+					</Button>
+				) : (
+					<Button
+						onClick={async () => {
+							await db.open();
+							db.unArchiveArticle(article.url);
+							setArticle({ ...article, archived: false });
+						}}
+					>
+						<ArchiveXIcon /> Continue reading
+					</Button>
+				)}
+			</div>
+		</>
 	);
 }
