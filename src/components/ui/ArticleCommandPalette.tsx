93dc25fc57e2393343aa8fac667e8e0509417c88
Isra <byzkk@protonmail.com>
2025-02-25 09:55:10 +0800

    Change text

--- src/components/ui/ArticleCommandPalette.tsx
+++ src/components/ui/ArticleCommandPalette.tsx
@@ -1,9 +1,4 @@
-import {
-	CommandDialog,
-	CommandInput,
-	CommandItem,
-	CommandList,
-} from '@/components/ui/command';
+import { CommandDialog, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
 import { Database } from '@/lib/database';
 import type { ArticleSaved } from '@/lib/types';
 import { checkUrlValid } from '@/lib/utils';
@@ -62,8 +57,8 @@ export function ArticleCommandPalette() {
 					(article) =>
 						article.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
 						article.subtitle.toLowerCase().includes(searchTerm.toLowerCase()) ||
-						article.author.toLowerCase().includes(searchTerm.toLowerCase())
-				)
+						article.author.toLowerCase().includes(searchTerm.toLowerCase()),
+				),
 			);
 		}
 	}, [searchTerm, articles]);
@@ -74,48 +69,44 @@ export function ArticleCommandPalette() {
 	};
 
 	return (
-		<CommandDialog
-			shouldFilter={false}
-			open={isOpen}
-			onOpenChange={(open) => setIsOpen(open)}
-		>
+		<CommandDialog shouldFilter={false} open={isOpen} onOpenChange={(open) => setIsOpen(open)}>
 			<CommandInput
 				value={searchTerm}
 				onValueChange={(value) => setSearchTerm(value)}
-				className="pr-6"
-				placeholder="Type a command or search..."
+				className='pr-6'
+				placeholder='Search or enter a URL...'
 			/>
-			<CommandList className="py-2">
+			<CommandList className='py-2'>
 				<CommandEmpty>
-					<div className="text-center my-8">No results found.</div>
+					<div className='text-center my-8'>No results found.</div>
 				</CommandEmpty>
 				<CommandGroup>
 					{filteredArticles.map((article) => (
 						<CommandItem
 							key={article.url}
 							onSelect={() => handleSelect(article)}
-							className="flex items-center space-x-2 py-2 cursor-pointer duration-100"
+							className='flex items-center space-x-2 py-2 cursor-pointer duration-100'
 						>
-							<div className="flex-shrink-0">
+							<div className='flex-shrink-0'>
 								{article.image ? (
 									<img
 										src={article.image}
 										alt={article.title}
-										className="size-8 object-cover rounded-md pointer-events-none"
+										className='size-8 object-cover rounded-md pointer-events-none'
 									/>
 								) : (
 									<img
 										src={article.authorImg}
 										alt={article.title}
-										className="size-8 object-cover rounded-md pointer-events-none"
+										className='size-8 object-cover rounded-md pointer-events-none'
 									/>
 								)}
 							</div>
-							<div className="flex-grow">
-								<h3 className="text-sm font-medium">{article.title}</h3>
-								<p className="text-xs text-muted-foreground line-clamp-1">
+							<div className='flex-grow'>
+								<h3 className='text-sm font-medium'>{article.title}</h3>
+								<p className='text-xs text-muted-foreground line-clamp-1'>
 									{article.author}
-									<DotIcon className="px-0 mx-0 inline-block" />
+									<DotIcon className='px-0 mx-0 inline-block' />
 									{article.subtitle}
 								</p>
 							</div>
@@ -128,8 +119,8 @@ export function ArticleCommandPalette() {
 								setIsOpen(false);
 							}}
 						>
-							<GlobeIcon className="size-8 text-muted-foreground" />
-							<span className="text-sm text-muted-foreground truncate line-clamp-1 overflow-hidden">
+							<GlobeIcon className='size-8 text-muted-foreground' />
+							<span className='text-sm text-muted-foreground truncate line-clamp-1 overflow-hidden'>
 								{searchTerm}
 							</span>
 						</CommandItem>
