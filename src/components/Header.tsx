2a8aaff0b2ad6e3e7d988e05c767d5a8100c8752
Isra <byzkk@protonmail.com>
2025-02-26 16:27:20 +0800

    Change home button to scroll up on index

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,17 +8,27 @@ import { useLocation } from 'react-router';
 
 export function Header() {
 	const location = useLocation();
-	const useHeader = location.pathname === '/' || location.pathname === '/archived';
+	const useHeader =
+		location.pathname === '/' || location.pathname === '/archived';
 
 	return (
-		<header className='md:sticky top-0 z-50 w-full border-b border-neutral-200 dark:border-neutral-800 p-2 dark:bg-neutral-950 bg-white print:hidden'>
-			<div className='flex justify-between'>
-				<div className='flex gap-4'>
-					<Link to='/' className={buttonVariants({ variant: 'outline', size: 'icon' })}>
-						<HomeIcon className='size-4' />
+		<header className="md:sticky top-0 z-50 w-full border-b border-neutral-200 dark:border-neutral-800 p-2 dark:bg-neutral-950 bg-white print:hidden">
+			<div className="flex justify-between">
+				<div className="flex gap-4">
+					<Link
+						to="/"
+						onClick={() => {
+							console.log(location.pathname);
+							if (location.pathname === '/') {
+								window.scrollTo({ top: 0, behavior: 'smooth' });
+							}
+						}}
+						className={buttonVariants({ variant: 'outline', size: 'icon' })}
+					>
+						<HomeIcon className="size-4" />
 					</Link>
 				</div>
-				<div className='flex gap-4'>
+				<div className="flex gap-4">
 					<ToggleMode />
 					{useHeader && <Formatting />}
 					<About />
