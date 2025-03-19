64b51d78883729c5faff2955c3900421dd1f0ae3
Isra <byzkk@protonmail.com>
2025-03-19 22:59:59 +0800

    Use tw-animate-css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,5 +93,5 @@ export default {
 			},
 		},
 	},
-	plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+	plugins: [require('@tailwindcss/typography')],
 } satisfies Config;
